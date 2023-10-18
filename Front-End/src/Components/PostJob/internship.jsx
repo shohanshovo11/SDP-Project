e0c4179588765947d70de38f4451fe809fbc4ddd
Waliza21 <145894902+Waliza21@users.noreply.github.com>
2023-10-18 12:30:16 +0600

    postJob

--- Front-End/src/Components/PostJob/internship.jsx
+++ Front-End/src/Components/PostJob/internship.jsx
@@ -1,34 +1,30 @@
 import axios from "axios";
 import { Navbar } from "../navbar/Navbar";
-import styles from "./PostJobtutor.module.css";
+import styles from "./Freelance.module.css";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
-const PostJobtutor = () => {
+const Internship = () => {
   const [title, setTitle] = useState("");
-  const [classs, setClass] = useState();
-  const [medium, setMedium] = useState("");
-  const [requiredTime, setrequiredTime] = useState();
-  const [salary, setsalary] = useState();
-  const [location, setlocation] = useState("");
-  const [subject, setsubject] = useState("");
+  const [position, setPosition] = useState("");
+  const [workingHour, setWorkingHour] = useState();
+  const [time, setTime] = useState();
+  const [salary, setSalary] = useState();
   const [description, setdescription] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const jobcount=await axios.get(`http://localhost:3002/tutorjobcount`);
+    const jobcount=await axios.get(`http://localhost:3002/internjobcount`);
     const reqbody={
          salary:salary, 
-         studentclass:classs,
-         version:medium,
+         startingTime:time,
+         position:position,
          description:description,
          id: `t${jobcount+1}`,
-         location:location,
-         subject:subject,
-         workingHour:requiredTime,
+         workingHour:workingHour,
          title:title
     }
-    const response=await axios.post(`http://localhost:3002/tutorjob`,reqbody);
+    const response=await axios.post(`http://localhost:3002/internjob`,reqbody);
     const data=response.data
     if(!data.acknowledged)
     {
@@ -36,6 +32,9 @@ const PostJobtutor = () => {
       return
     }
     toast.success("Data successfully inserted")
+    setTimeOut(() =>{
+      window.location.reload(true)
+    },1000)
   };
   return (
     <div className={styles.postJobtutor} >
@@ -45,12 +44,10 @@ const PostJobtutor = () => {
       <h1 className={styles.a}>a</h1>
       <h1 className={styles.job}>Job</h1>
       <div className={styles.title}>Title</div>
-      <div className={styles.class}>Class</div>
-      <div className={styles.medium}>Medium</div>
-      <div className={styles.requiredTime}>Required Time</div>
+      <div className={styles.class}>Position</div>
+      <div className={styles.requiredTime}>Working Hour</div>
+      <div className={styles.medium}>Starting Time</div>
       <div className={styles.salary}>Salary</div>
-      <div className={styles.location}>Location</div>
-      <div className={styles.subject}>Subject</div>
       <div className={styles.description}>Description</div>
       <form onSubmit={handleSubmit}>
         <input
@@ -64,23 +61,23 @@ const PostJobtutor = () => {
         <input
           required
           onChange={(e) => {
-            setClass(e.target.value);
+            setPosition(e.target.value);
           }}
           className={styles.postJobtutorInner}
-          type="number"
-        />
-        <input
-          required
-          onChange={(e) => {
-            setMedium(e.target.value);
-          }}
-          className={styles.rectangleInput}
           type="text"
         />
         <input
           required
           onChange={(e) => {
-            setrequiredTime(e.target.value);
+            setWorkingHour(e.target.value);
+          }}
+          className={styles.rectangleInput}
+          type="number"
+        />
+        <input
+          required
+          onChange={(e) => {
+            setTime(e.target.value);
           }}
           className={styles.postJobtutorChild1}
           type="number"
@@ -88,26 +85,10 @@ const PostJobtutor = () => {
         <input
           required
           onChange={(e) => {
-            setsalary(e.target.value);
+            setSalary(e.target.value);
           }}
           className={styles.postJobtutorChild2}
           type="number"
-        />
-        <input
-          required
-          onChange={(e) => {
-            setlocation(e.target.value);
-          }}
-          className={styles.postJobtutorChild3}
-          type="text"
-        />
-        <input
-          required
-          onChange={(e) => {
-            setsubject(e.target.value);
-          }}
-          className={styles.postJobtutorChild4}
-          type="text"
         />
         <input
           onChange={(e) => {
@@ -125,4 +106,4 @@ const PostJobtutor = () => {
   );
 };
 
-export default PostJobtutor;
+export default Internship;
