f3c0a74014d97bf332e175de5fc0516c3c8dab72
Tabassum-Nishat <144160556+Tabassum-Nishat@users.noreply.github.com>
2023-10-24 23:06:05 +0600

    Added The applicant list frontend

---
+++ Front-End/src/Components/ApplicantList/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./ApplicantList.css";
+import profile from "../Images/profile.png";
+
+export const Card = (props) => {
+  return (
+    <div>
+      <div className="card">
+        <img className="profile-image" src={profile} alt="Profile image" />
+        <h5 className="name">{props.name}</h5>
+        <span className="p-description">{props.description}</span>
+        <div className="qualification">
+          <span className="tag">{props.tags[0]}</span>
+          <span className="tag">{props.tags[1]}</span>
+          <span className="tag">{props.tags[2]}</span>
+        </div>
+        <div className="buttons">
+          <button className="viewButton">View Profile</button>
+          <button className="acceptButton">Accept</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
