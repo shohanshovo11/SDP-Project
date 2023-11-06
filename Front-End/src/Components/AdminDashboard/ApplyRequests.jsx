cac6fa0dfa489ac39091c62c8237be4a1c36e291
RafsanProve <124572117+RafsanProve@users.noreply.github.com>
2023-11-06 15:25:19 +0600

    Delete Front-End directory

--- Front-End/src/Components/AdminDashboard/ApplyRequests.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ApplyRequests() {
-  return (
-    <div className="w-full font-poppins">
-      <div className=" text-black text-5xl">Hello from Page 2</div>
-    </div>
-  );
-}
