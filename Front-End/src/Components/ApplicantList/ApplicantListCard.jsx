891e4f197597767d2114f94d725229a60ca769e2
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2023-11-17 01:41:07 +0600

    shohan 3.0

--- Front-End/src/Components/ApplicantList/ApplicantListCard.jsx
+++ Front-End/src/Components/ApplicantList/ApplicantListCard.jsx
@@ -17,20 +17,35 @@ export const ApplicantListCard = (props) => {
     openModal();
   };
   return (
-    <div className="card">
-      <img className="profile-image" src={profile} alt="Profile image" />
-      <h5 className="name">{props.name}</h5>
-      <span className="p-description">{props.description}</span>
-      <div className="qualification">
-        <span className="tag">{props.tags[0]}</span>
-        <span className="tag">{props.tags[1]}</span>
-        <span className="tag">{props.tags[2]}</span>
+    <div className="w-56 h-64 bg-[#8dffd041] flex flex-col justify-between items-center rounded-md shadow-lg shadow-slate-400">
+      <img
+        className="rounded-full w-16 pt-4"
+        src={profile}
+        alt="Profile image"
+      />
+      <h5 className="">{props.name}</h5>
+      <span className="">{props.description}</span>
+      <div className="flex gap-2 mb-2 mt-2">
+        <span className="bg-slate-500 text-white rounded-3xl px-2">
+          {props.tags[0]}
+        </span>
+        <span className="bg-slate-500 text-white rounded-3xl px-2">
+          {props.tags[1]}
+        </span>
+        <span className="bg-slate-500 text-white rounded-3xl px-2">
+          {props.tags[2]}
+        </span>
       </div>
-      <div className="buttons mb-3">
-        <button className="viewButton" onClick={handleViewProfile}>
+      <div className="mb-4 flex gap-7">
+        <button
+          className="bg-bt text-white rounded-3xl px-2 py-1"
+          onClick={handleViewProfile}
+        >
           View Profile
         </button>
-        <button className="acceptButton">Accept</button>
+        <button className="bg-bt text-white rounded-3xl px-2 py-1">
+          Accept
+        </button>
       </div>
       <ApplicantProfile closeModal={closeModal} isModalOpen={isModalOpen} />
     </div>
