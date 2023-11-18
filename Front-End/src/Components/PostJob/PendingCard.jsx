fc19d17c49cc445607a0fa63b9ecbfb796fb40fc
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2023-11-18 21:30:23 +0600

    added profile Modal on employer dashboard

--- Front-End/src/Components/PostJob/PendingCard.jsx
+++ Front-End/src/Components/PostJob/PendingCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./PendingCard.css";
 import { toast } from "react-toastify";
 import { Axios } from "../api/api";
+import ModalProfile from "./ModalProfile";
 
 function PendingCard({ pendingjob }) {
   async function insertpendingjob() {
@@ -14,15 +15,32 @@ function PendingCard({ pendingjob }) {
     toast.success("Data successfully inserted");
   }
   return (
-    <div className="guava">
-      <div className="kola">{pendingjob.title}</div>
-
-      <div className="komola">Working Hour: {pendingjob.workingHour}</div>
-      <div className="ranoutofname">Desc: {pendingjob.description}</div>
-      <button className="peyara" onClick={insertpendingjob}>
-        Approve
-      </button>
-    </div>
+    <>
+      <div className="guava">
+        <div className="kola">{pendingjob.title}</div>
+        {/* {console.log(pendingjob.rate ? pendingjob.rate : pendingjob.salary)} */}
+        <div className="komola">Working Hour: {pendingjob.workingHour}</div>
+        <div className="ranoutofname">Desc: {pendingjob.description}</div>
+        <div>
+          Payment: {pendingjob.rate ? pendingjob.rate : pendingjob.salary}
+        </div>
+        <div className="flex gap-4 w-full justify-between">
+          <button
+            className="peyara"
+            onClick={() =>
+              document.getElementById("admin_view_profile_modal").showModal()
+            }
+          >
+            View Profile
+          </button>
+          <button className="peyara" onClick={insertpendingjob}>
+            Approve
+          </button>
+          <button className="peyara">Reject</button>
+        </div>
+      </div>
+      <ModalProfile />
+    </>
   );
 }
 
