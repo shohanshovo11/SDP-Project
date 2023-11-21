7489b97427f48e858f52ff5b9d31af91c224c9d8
RafsanProve <124572117+RafsanProve@users.noreply.github.com>
2023-11-22 00:31:16 +0600

    Applicant List Updated

--- Front-End/src/Components/ApplicantList/ApplicantList.jsx
+++ Front-End/src/Components/ApplicantList/ApplicantList.jsx
@@ -1,137 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Dna } from "react-loader-spinner";
+import { Axios } from "../api/api";
+import axios from "axios";
+import { JobItem } from "./JobItem";
 import "./ApplicantList.css";
-import { ApplicantListCard } from "./ApplicantListCard";
-const applicantsData = [
-  {
-    name: "Sadiqul Alom",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Sabbir",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Nasif",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Rafsan",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Waliza",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Nishat",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Shohan",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Applicant 1",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Abdullah",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Nahid",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Rifat",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-  {
-    name: "Shovo",
-    description: "Description 1",
-    tags: ["Tag 1", "Tag 2", "Tag 3"],
-  },
-];
+
+
 export const ApplicantList = () => {
-  const itemsPerPage = 6; // Number of applicants to display per page
-  const [currentPage, setCurrentPage] = useState(1);
+  const tokenAvailable = !!localStorage.getItem("token");
+  const [loading, setLoading] = useState(false);
+  const email = localStorage.getItem("email");
 
-  const indexOfLastApplicant = currentPage * itemsPerPage;
-  const indexOfFirstApplicant = indexOfLastApplicant - itemsPerPage;
-  const currentApplicants = applicantsData.slice(
-    indexOfFirstApplicant,
-    indexOfLastApplicant
-  );
-  const [activeJob, setActiveJob] = useState("Job 1"); // Default active job
+  const apiUrl = "/employers-joblist"; // Adjust the URL as needed
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  const handleJobClick = (job) => {
-    setActiveJob(job === activeJob ? null : job);
-  };
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5000/employers-joblist/${email}`)
+      .then((response) => {
+        const data = response.data;
+        setRecords(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching jobs:", error);
+      });
+  });
   return (
     <>
-      <div className="h-full font-poppins overflow-x-hidden">
-        <div className="flex">
-          <div className="pt-12 pl-12 flex flex-col w-96 sticky">
-            <h1 className="text-4xl pb-4 font-bold text-center">Live Jobs</h1>
-            {["Job 1", "Job 2", "Job 3", "Job 4", "Job 5"].map((job, index) => (
-              <button
-                key={index}
-                onClick={() => handleJobClick(job)}
-                className={`btn rounded-md h-8 mb-2 hover:bg-bt hover:text-white ${
-                  activeJob === job ? "bg-bt text-white " : "bg-slate-300 "
-                }`}
-              >
-                {job}
-              </button>
-            ))}
-          </div>
-
-          <div className="pr-10 pl-12">
-            <h1 className="text-4xl font-bold text-center pt-12 pb-4">
-              Applicants
-            </h1>
-            <div className="whole-card grid grid-cols-3 gap-4">
-              {currentApplicants.map((applicant, index) => (
-                <ApplicantListCard
-                  key={index}
-                  name={applicant.name}
-                  description={applicant.description}
-                  tags={applicant.tags}
-                />
-              ))}
-            </div>
-            <div className="pagination flex gap-2 mt-6">
-              {Array.from(
-                { length: Math.ceil(applicantsData.length / itemsPerPage) },
-                (_, index) => (
-                  <button
-                    key={index}
-                    onClick={() => paginate(index + 1)}
-                    className={`btn-pagination hover:text-black btn-circle text-black ${
-                      currentPage === index + 1
-                        ? "btn bg-bt active text-white"
-                        : "btn bg-slate-300"
-                    }`}
-                  >
-                    {index + 1}
-                  </button>
-                )
-              )}
-            </div>
-          </div>
-        </div>
+      <div className="h-full w-full flex flex-row gap-x-8 px-4 py-4 font-poppins overflow-x-hidden">
+          {records.map((job, index) => (
+            <JobItem
+              key={index}
+              jobitem={{
+                title: job.title,
+                tags: job.tags,
+                salary: job.salary,
+              }}
+            />
+          ))}
       </div>
     </>
   );
