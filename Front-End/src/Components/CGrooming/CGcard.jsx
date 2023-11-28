271f3a11bd4f1b427df0e5b9f991b7b06fb0c7ac
Tabassum-Nishat <144160556+Tabassum-Nishat@users.noreply.github.com>
2023-11-28 20:24:05 +0600

    cgfe up

--- Front-End/src/Components/CGrooming/CGcard.jsx
+++ Front-End/src/Components/CGrooming/CGcard.jsx
@@ -3,22 +3,20 @@ import "./CGrooming.css";
 
 export const CGcard = (channel) => {
   return (
-    <div>
-      <div className="card">
-        <img
-          className="profile-image"
-          src={channel.profileImgUrl}
-          alt="Profile image"
-        />
-        <h5 className="Name">{channel.name}</h5>
-        <span className="Name">{channel.vidcount}</span>
-        <div className="vidtag">
-          <span className="tags">{channel.tag[0]}</span>
-          <span className="tags">{channel.tag[1]}</span>
-          <span className="tags">{channel.tag[2]}</span>
-        </div>
-        <button className="viewButton">View Courses</button>
+    <div className="card">
+      <img
+        className="profile-image"
+        src={channel.profileImgUrl}
+        alt="Profile image"
+      />
+      <h5 className="Name">{channel.name}</h5>
+      <span className="Name">{channel.vidcount}</span>
+      <div className="vidtag">
+        <span className="tags">{channel.tag[0]}</span>
+        <span className="tags">{channel.tag[1]}</span>
+        <span className="tags">{channel.tag[2]}</span>
       </div>
+      <button className="viewButton">View Courses</button>
     </div>
   );
 };
