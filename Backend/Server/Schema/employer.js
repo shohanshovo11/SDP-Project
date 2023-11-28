7f66ca4e248fa028b2d8aeedf1e112bdd72d2760
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2023-11-29 03:37:59 +0600

    fix and employer live jobs changed

--- Backend/Server/Schema/employer.js
+++ Backend/Server/Schema/employer.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const employerSchema = new mongoose.Schema({
   email: { type: String, required: true },
   name: { type: String, required: true },
@@ -10,9 +11,11 @@ const employerSchema = new mongoose.Schema({
   occupation: { type: String },
   position: { type: String },
   experience: { type: Number },
-  gender: { type: String }
+  gender: { type: String },
+  profileImgUrl: { type: String } // Add this field for base64 profile image
 });
 
 // Create a User model
 const Employer = mongoose.model('employers', employerSchema);
+
 module.exports = Employer;
